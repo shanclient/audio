f3dd21cf110e577f82cd348a3f0b85372e717a20
shanclient <141842941+shanclient@users.noreply.github.com>
2023-08-29 16:05:58 +0530

    Create start.py

---
+++ start.py
@@ -0,0 +1,29 @@
+import subprocess
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Send me a video to extract audio.')
+
+def process_video(update: Update, context: CallbackContext) -> None:
+    video_file = update.message.video.get_file()
+    video_path = video_file.download()
+
+    audio_file = "extracted_audio.mp3"
+    subprocess.run(["ffmpeg", "-i", video_path, "-vn", audio_file])
+
+    with open(audio_file, "rb") as audio:
+        update.message.reply_audio(audio)
+
+def main():
+    updater = Updater("YOUR_BOT_TOKEN")
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.video, process_video))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
